6a377dd7adac16d2ddc56c5e29f3385c0d4c8802
Demetrius Chatterjee <dchatterhee3@dons.usfca.edu>
2025-05-05 19:23:18 -0700

    adding javadoc

--- src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndexBuilder.java
+++ src/main/java/edu/usfca/cs272/ThreadSafeInvertedIndexBuilder.java
@@ -5,19 +5,40 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+
 /**
  * A thread-safe implementation of InvertedIndexBuilder that uses a WorkQueue for
  * parallel processing of files and directories. This implementation parallelizes
  * both file processing and directory traversal for improved performance.
  */
 public class ThreadSafeInvertedIndexBuilder extends InvertedIndexBuilder {
+    /**
+     * The work queue for parallel processing.
+     */
     private final WorkQueue queue;
-
+    /**
+     * The inverted index to build.
+     */
+    private final ThreadSafeInvertedIndex index;
+    /**
+     * Constructs a new ThreadSafeInvertedIndexBuilder with the given index and work queue.
+     *
+     * @param index the inverted index to build
+     * @param queue the work queue for parallel processing
+     */
     public ThreadSafeInvertedIndexBuilder(ThreadSafeInvertedIndex index, WorkQueue queue) {
         super(index);
         this.queue = queue;
+        this.index = index;
     }
 
+    /**
+     * Builds the index from a file by processing its contents.
+     *
+     * @param path the input path to process
+     * @throws IOException if an IO error occurs during file processing
+     */
     @Override
     public void buildFile(Path path) throws IOException {
         super.buildFile(path);
