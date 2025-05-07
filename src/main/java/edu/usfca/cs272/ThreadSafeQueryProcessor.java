0b60ab250b82350ce5ea078a2c396ee29ffa06c0
Demetrius Chatterjee <dchatterhee3@dons.usfca.edu>
2025-05-06 19:59:28 -0700

    removing extends

--- src/main/java/edu/usfca/cs272/ThreadSafeQueryProcessor.java
+++ src/main/java/edu/usfca/cs272/ThreadSafeQueryProcessor.java
@@ -1,13 +1,19 @@
 package edu.usfca.cs272;
 
+import static opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
 
 /**
  * A thread-safe implementation of QueryProcessor that uses a WorkQueue for parallel
@@ -17,12 +23,24 @@ import java.util.TreeSet;
  * @author Demetrius Chatterjee
  * @version Spring 2025
  */
-public class ThreadSafeQueryProcessor extends QueryProcessor {
-    /** The work queue for parallel processing of queries. */
-    private final WorkQueue queue;
+public class ThreadSafeQueryProcessor {
+    /** The map to store exact search results for each query. */
+    private final TreeMap<String, List<InvertedIndex.SearchResult>> allResultsExact;
+    
+    /** The map to store partial search results for each query. */
+    private final TreeMap<String, List<InvertedIndex.SearchResult>> allResultsPartial;
+
+    /** The stemmer to use for stemming query words. */
+    private final SnowballStemmer stemmer;
+
+    /** The inverted index to store the results of the search to. */
+    private final ThreadSafeInvertedIndex index;
 
     /** Whether to use partial search. */
     private final boolean usePartialSearch;
+
+    /** The work queue for parallel processing of queries. */
+    private final WorkQueue queue;
 
     /**
      * Initializes a thread-safe query processor with the default number of worker threads.
@@ -32,9 +50,22 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
      * @param queue the work queue to use for parallel processing
      */
     public ThreadSafeQueryProcessor(ThreadSafeInvertedIndex index, boolean usePartialSearch, WorkQueue queue) {
-        super(index, usePartialSearch);
-        this.queue = queue;
+        this.allResultsExact = new TreeMap<>();
+        this.allResultsPartial = new TreeMap<>();
+        this.stemmer = new SnowballStemmer(ENGLISH);
+        this.index = index;
         this.usePartialSearch = usePartialSearch;
+        this.queue = queue;
+    }
+
+    /**
+     * Processes a single line of query text into a sorted list of unique stems.
+     *
+     * @param line the line of query text to process
+     * @return a sorted TreeSet of unique stems from the processed line
+     */
+    public TreeSet<String> processLine(final String line) {
+        return FileStemmer.uniqueStems(line, stemmer);
     }
 
     /**
@@ -44,26 +75,32 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
      * @param line the query line to process
      * @return a list of search results from the inverted index
      */
-    @Override
     public List<InvertedIndex.SearchResult> processQueryLine(String line) {
+        // Process the line into stems
         synchronized (this) {
-            // Process the line into stems
-            TreeSet<String> stems = processLine(line);
-            if (stems.isEmpty()) {
-                return super.processQueryLine(line);
-            }
-
-            // Get the query string
-            String queryString = getQueryString(stems);
-            
+        TreeSet<String> stems = processLine(line);
+        if (stems.isEmpty()) {
+            return Collections.emptyList();
+        }
+        //synchronized (currentResults) {
+        // Get the query string
+        String queryString = getQueryString(stems);
+        
+        // Get the current results
+        var currentResults = getResults(usePartialSearch);
+        //synchronized (currentResults) {
             // Check if we already have results for this query
-            List<InvertedIndex.SearchResult> existingResults = super.getSearchResult(queryString, usePartialSearch);
-            if (existingResults != null) {
-                return existingResults;
+            if (currentResults.containsKey(queryString)) {
+                return currentResults.get(queryString);
             }
 
-            // Process the query
-            return super.processQueryLine(line);
+            // Search the index
+            List<InvertedIndex.SearchResult> results = index.search(stems, usePartialSearch);
+        //synchronized (currentResults) {    
+            // Store the results
+            currentResults.put(queryString, results);
+            
+            return results;
         }
     }
 
@@ -74,7 +111,6 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
      * @param path the path to the query file
      * @throws IOException if unable to read or process the query file
      */
-    @Override
     public void processQueryFile(Path path) throws IOException {
         try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
             String line;
@@ -84,29 +120,17 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
             }
         }
         // Wait for all tasks to complete
-        queue.join();
+        queue.finish();
     }
 
     /**
-     * Thread-safe implementation of getting search result keys.
+     * Returns the query string from the given stems.
      *
-     * @return an unmodifiable view of the search result keys
+     * @param stems the stems to use
+     * @return the query string
      */
-    @Override
-    public Set<String> getSearchResultKeys() {
-        return super.getSearchResultKeys();
-    }
-
-    /**
-     * Thread-safe implementation of getting search results for a query.
-     *
-     * @param queryString the query string to get results for
-     * @param usePartialSearch whether to use partial search results
-     * @return an unmodifiable view of the search results, or null if no results exist
-     */
-    @Override
-    public List<InvertedIndex.SearchResult> getSearchResult(String queryString, boolean usePartialSearch) {
-        return super.getSearchResult(queryString, usePartialSearch);
+    public static String getQueryString(TreeSet<String> stems) {
+        return String.join(" ", stems);
     }
 
     /**
@@ -116,8 +140,65 @@ public class ThreadSafeQueryProcessor extends QueryProcessor {
      * @param usePartialResults whether to use partial search
      * @throws IOException if an IO error occurs
      */
-    @Override
     public void toJson(Path path, boolean usePartialResults) throws IOException {
-        super.toJson(path, usePartialResults);
+        var results = getResults(usePartialResults);
+        synchronized (results) {
+            JsonWriter.writeSearchResults(results, path);
+        }
+    }
+
+    /**
+     * Returns the results for the given usePartialResults.
+     * 
+     * @param usePartialResults whether to use partial search
+     * @return the results for the given usePartialResults
+     */
+    private TreeMap<String, List<InvertedIndex.SearchResult>> getResults(boolean usePartialResults) {
+        return usePartialResults ? allResultsPartial : allResultsExact;
+    }
+
+    /**
+     * Returns a string representation of the QueryProcessor object.
+     *
+     * @return a string representation of the QueryProcessor object
+     */
+    @Override
+    public String toString() {
+        var results = getResults(usePartialSearch);
+        synchronized (results) {
+            return String.format("ThreadSafeQueryProcessor[exact=%d, partial=%d]", 
+                allResultsExact.toString(), allResultsPartial.toString());
+        }
+    }
+
+    /**
+     * Returns an unmodifiable view of the search result keys.
+     * 
+     * @return an unmodifiable view of the search result keys
+     */
+    public Set<String> getSearchResultKeys() {
+        var results = getResults(usePartialSearch);
+        synchronized (results) {
+            return Collections.unmodifiableSet(results.keySet());
+        }
+    }
+
+    /**
+     * Returns an unmodifiable view of the search results for a given query string.
+     * 
+     * @param queryString the query string to get results for
+     * @param usePartialSearch whether to use partial search results
+     * @return an unmodifiable view of the search results, or null if no results exist
+     */
+    public List<InvertedIndex.SearchResult> getSearchResult(String queryString, boolean usePartialSearch) {
+        // Process the query string to ensure consistent format
+        TreeSet<String> stems = processLine(queryString);
+        String processedQuery = getQueryString(stems);
+        
+        var results = getResults(usePartialSearch);
+        synchronized (results) {
+            List<InvertedIndex.SearchResult> searchResults = results.get(processedQuery);
+            return searchResults != null ? Collections.unmodifiableList(searchResults) : null;
+        }
     }
 }
